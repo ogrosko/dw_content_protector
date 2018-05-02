a138b9c75d981be0f82789188a4bc2277febf558
Ondrej Grosko <ondrej@digitalwerk.agency>
2018-05-02 12:38:19 +0200

    TER upload prepare

--- Classes/Hooks/TCEmainHook.php
+++ Classes/Hooks/TCEmainHook.php
@@ -1,16 +1,29 @@
 <?php
 namespace Digitalwerk\DwContentProtector\Hooks;
 
-/***
- *
- * This file is part of the "Boilerplate" Extension for TYPO3 CMS.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
+/***************************************************************
+ *  Copyright notice
  *
  *  (c) 2017 Ondrej Grosko <ondrej@digitalwerk.agency>, Digitalwerk
  *
- ***/
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * TYPO3 TCE hook class used for DataHandler hooks
--- ext_emconf.php
+++ ext_emconf.php
@@ -2,22 +2,28 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Flux content protector',
-    'description' => 'Try to fix this bug https://github.com/FluidTYPO3/flux/issues/1306',
-    'category' => 'plugin',
+    'description' => 'Protects flux content elements to be created/moved/copied to restricted column. Try to fix this bug https://github.com/FluidTYPO3/flux/issues/1306',
+    'category' => 'be',
     'author' => 'Ondrej Grosko',
     'author_email' => 'ondrej@digitalwerk.agency',
-    'state' => 'beta',
+    'author_company' => 'Digitalwerk',
+    'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '0.0.1',
+    'version' => '0.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '8.6.0-8.7.99',
-            'flux' => '*',
+            'flux' => '> 8.0.0',
         ],
         'conflicts' => [],
         'suggests' => [],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Digitalwerk\\DwContentProtector\\' => 'Classes',
+        ]
     ],
 ];
